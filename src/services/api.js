970720d7bac7f8a1d77893387c2c47b6b39af816
Aliaksei Iuko <alexivko01@gmail.com>
2024-03-11 08:59:47 +0100

    Fixed bugs

--- src/services/api.js
+++ src/services/api.js
@@ -27,7 +27,7 @@ export async function fetchData(offset) {
 
     try {
         const response = await fetch(
-            'https://api.valantis.store:40000/',
+            'http://api.valantis.store:40000/',
             request
         )
         if (!response.ok) {
